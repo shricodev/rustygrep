d57ed6ccf6c3357339ebb51ea710bf5e8cf5f700
Shrijal Acharya <shrijal.acharya@gmail.com>
2024-01-20 19:42:20 +0545

    feat(io-stream): Change the display of error from output to err stream

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let initial_config = InitialConfig::build(&args).unwrap_or_else(|_err| {
+    let initial_config = InitialConfig::build(&args).unwrap_or_else(|err| {
+        eprintln!("Error: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = rustygrep::run(initial_config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
