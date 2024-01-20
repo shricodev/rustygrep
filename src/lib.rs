d21d302dd47403ba3f7ac9222b6b9b5f59f6e405
Shrijal Acharya <shrijal.acharya@gmail.com>
2024-01-20 16:57:29 +0545

    feat(case): Add feature to be able to search on the file in CASE_INSENSITIVE mode with IGNORE_CASE env var

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,44 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct InitialConfig<'a> {
     pub search_string: &'a str,
     pub file_path: &'a str,
+    pub ignore_case: bool,
 }
 
 impl<'a> InitialConfig<'a> {
     pub fn build(args: &[String]) -> Result<InitialConfig, &str> {
-        // ["executable_path", "search_string", "file_name"] => This is the output for the args variable.
+        // ["executable_path", "search_string", "file_name"] => This is the content for the args variable.
         if args.len() < 3 {
-            return Err("Not enough arguments. Please provide a search string and a file name.");
+            return Err(
+                "Not enough arguments. Please provide a search string and a file name. eg: 'rustygrep search_string file_name'",
+            );
         }
         let search_string = &args[1];
         let file_path = &args[2];
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         Ok(InitialConfig {
             search_string,
             file_path,
+            ignore_case,
         })
     }
 }
 
 pub fn run(initial_config: InitialConfig) -> Result<(), Box<dyn Error>> {
     let file_contents = fs::read_to_string(initial_config.file_path)?;
-    for line in search(&initial_config.search_string, &file_contents) {
+
+    let results = if initial_config.ignore_case {
+        search_case_insensitive(&initial_config.search_string, &file_contents)
+    } else {
+        search(&initial_config.search_string, &file_contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
     Ok(())
@@ -39,18 +54,46 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "rust";
         let contents = "\
-        rust: safe, fast, productive.";
+rust: safe, fast, productive.
+Rust: This line should not be returned.";
         assert_eq!(
             vec!["rust: safe, fast, productive."],
             search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUSt";
+        let contents = "\
+Rust: safe, fast, productive.
+rust: This line should also be returned.";
+        assert_eq!(
+            vec![
+                "Rust: safe, fast, productive.",
+                "rust: This line should also be returned."
+            ],
+            search_case_insensitive(query, contents)
         );
     }
 }
